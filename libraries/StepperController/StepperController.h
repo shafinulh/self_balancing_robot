66efaa5c1083faaed04f5d091488ca39ebc9510b
shafinulh <haqueshafinul@gmail.com>
2022-08-04 21:56:54 -0400

    Update README.md for libraries used in project

--- libraries/StepperController/StepperController.h
+++ libraries/StepperController/StepperController.h
@@ -4,12 +4,6 @@
 #include <String.h>
 #include <Servo.h> 
 
-class Utils{
-public: 
-  static double map(double min1, double max1, double min2, double max2, double val);
-  static double bound(double val, double min, double max);
-};
-
 class StepperMotor{
 //-----------------------------------------------------------------------------------------------------------
 //Variable Declaration
